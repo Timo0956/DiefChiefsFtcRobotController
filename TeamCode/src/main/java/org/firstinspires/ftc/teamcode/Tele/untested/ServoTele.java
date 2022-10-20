bc76b137803a97047a5bc58e497167ee686e7a6a
Timo <83043823+Timo0956@users.noreply.github.com>
2022-10-20 15:55:49 -0600

    Create ServoTele.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/ServoTele.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Tele.untested;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+//edit if required to adapt to 2 servos
+public class ServoTele extends LinearOpMode {
+    static Servo ClawL; 
+   // static Servo ClawR;
+    static double startPos = 1;
+    static double closePos = 0.2; // adjust maybe
+    //static servoController sCont = null;
+    static ElapsedTime runTime = null;
+    
+    //initializing servos
+    public static void setServos(Servo Cl/*, Servo Cr*/){
+        ClawL = Cl;
+        //ClawR = Cr;
+        open();
+        
+        sCont = ClawL.getController(); //should declare for both servos
+        
+        sCont.pwmEnable(); //enabling just in case
+        
+        runTime = new ElapsedTime();
+        
+    }
+    
+    //releases object when x is pressed
+    public static void release(boolean x){
+        if(ClawL.getPosition() == -closePos /*&& ClawR.getPosition() == closePos*/ && x){
+             open();  
+        }
+    }
+    //holds object when y is pressed
+    public static void hold(boolean y){
+        if(ClawL.getPosition() == -startPos /*&& ClawR.getPosition() == startPos*/ && y){
+            close();
+        }
+    }
+    
+    public static void open(){
+        ClawL.setPosition(-startPos);
+    //    ClawR.setPosition(startPos);
+    }
+    
+    public static void close(){
+        ClawL.setPosition(-closePos);
+   //     ClawR.setPosition(closePos);
+    }
+}
