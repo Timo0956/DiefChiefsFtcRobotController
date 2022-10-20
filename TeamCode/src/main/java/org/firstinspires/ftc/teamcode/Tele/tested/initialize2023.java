f2451d7d7867e824ac8010a6b3089aa2895f4222
Timo <83043823+Timo0956@users.noreply.github.com>
2022-10-20 15:57:38 -0600

    Create initialize2023.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/tested/initialize2023.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.Tele.tested;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Tele.untested.ServoTele;
+import org.firstinspires.ftc.teamcode.Tele.untested.TwoStageLinSlideFile;
+
+public class initialize {
+
+    private DcMotor motorFrontLeft= null;
+    private DcMotor motorFrontRight = null;
+    private DcMotor motorBackLeft = null;
+    private DcMotor motorBackRight = null;
+    private DcMotor LinSlideMotor = null;
+    private Servo dumpServo = null;
+
+    HardwareMap hardwareMap= null;
+
+
+    public initialize(HardwareMap HRDWRMAP){
+        hardwareMap = HRDWRMAP;
+    }
+  /*  public void initDT(){
+        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        drivechain.setDTMotors(motorFrontLeft,motorFrontRight,motorBackLeft,motorBackRight);
+    } */
+    
+  public void initLSright(){
+        LinSlideMotorR = hardwareMap.dcMotor.get("LinSlideMotorR");
+        TwoStageLinSlideFile.setLSMotor(LinSlideMotorR);
+    }
+
+    public void initClaw(){
+        clawServo = hardwareMap.servo.get("clawServo");
+        dump.setDumpServo(dumpServo);
+    }
+
+
+
+
+}
