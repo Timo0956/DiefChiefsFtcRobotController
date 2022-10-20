c8251ef0425f70ef69d60bf053f87d09f2f6fce2
Timo <83043823+Timo0956@users.noreply.github.com>
2022-10-20 15:56:26 -0600

    Create TwoStageLinSlide.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/TwoStageLinSlide.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.Tele.untested;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+@TeleOp
+public class TwoStageLinSlide extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException{
+        DcMotor rightLinSlide = hardwareMap.dcMotor.get("rightLinSlide"); //defines our motors for LinSlide
+        //DcMotor leftLinSlide = hardwareMap.dcMotor.get("leftLinSlide");
+
+        waitForStart();
+        while (opModeIsActive()){
+            TwoStageLinSlideFile.setLSMotor(rightLinSlide/*, leftLinSlide*/); //defines motors in terms of the seperate file
+            TwoStageLinSlideFile.linSlideDouble(gamepad1); //takes gamepad input
+            ServoTele.setServos(ClawServo);
+            ServoTele.release(gamepad1.x);
+            ServoTele.close(gamepad1.y);
+        }
+    }
+}
