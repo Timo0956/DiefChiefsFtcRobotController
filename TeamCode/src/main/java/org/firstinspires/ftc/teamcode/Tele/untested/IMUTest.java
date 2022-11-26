87077c104f7c8b399162b27212d3529a83e332b7
vityism <wz2684@gmail.com>
2022-11-25 23:43:32 -0700

    imu and acc test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/IMUTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/IMUTest.java
@@ -3,10 +3,14 @@ package org.firstinspires.ftc.teamcode.Tele.untested;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
 import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 import java.util.Locale;
@@ -14,38 +18,104 @@ import java.util.Locale;
 @TeleOp
 public class IMUTest extends LinearOpMode {
 
+    static DcMotor motorFrontLeft = null;
+    static DcMotor motorBackLeft = null;
+    static DcMotor motorFrontRight = null;
+    static DcMotor motorBackRight = null;
+    final static double power = 1;
+
     BNO055IMU imu;
     Orientation angles;
-    // Acceleration gravity;
-
+    Acceleration gravity;
 
     @Override
     public void runOpMode() throws InterruptedException{
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
         parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-         // parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+
+        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        motorFrontRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorFrontLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorBackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
 
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
 
+        gravity = imu.getLinearAcceleration();
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        telemetry.addData("IMU Status", imu.getSystemStatus());
+        telemetry.addData("Calibration Status", imu.getCalibrationStatus());
+
+        telemetry.update();
+
         waitForStart();
 
-        while(opModeIsActive())
-        {
-            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-            telemetry.addData("IMU Status", imu.getSystemStatus());
-            telemetry.addData("Calibration Status", imu.getCalibrationStatus());
-            telemetry.addData("Heading (Z)", angles.firstAngle); //Z
-            telemetry.addData("Roll (Y)", angles.secondAngle); // Y
-            telemetry.addData("Pitch (X)", angles.thirdAngle); // X
+        while(opModeIsActive()) {
 
-            telemetry.update();
+            //turn(power);
+            //moveForwardBack(power);
+            //moveSideways(power);
+
 
         }
 
 
+    }
+    public void turn(double power) throws InterruptedException{
+        motorFrontLeft.setPower(-power);
+        motorBackLeft.setPower(-power);
+        motorFrontRight.setPower(power);
+        motorBackRight.setPower(power);
 
+        for(int i=0; i<40; i++){
+            telemetry.addData("Heading (Z)"+i*50, angles.firstAngle); //Z
+            telemetry.addData("Roll (Y)"+i*50, angles.secondAngle); // Y
+            telemetry.addData("Pitch (X)"+i*50, angles.thirdAngle); // X
+            telemetry.update();
+            Thread.sleep(50);
 
+        }
+    }
+
+    public void moveForwardBack(double power) throws InterruptedException{
+        motorFrontLeft.setPower(power);
+        motorBackLeft.setPower(power);
+        motorFrontRight.setPower(power);
+        motorBackRight.setPower(power);
+
+        for(int i=0; i<40; i++){
+            telemetry.addData("Forward(Z)"+i*50, gravity.zAccel); //Z
+//            telemetry.addData("Sideways(Y)"+i*50, gravity.yAccel); // Y
+//            telemetry.addData("Up (X)"+i*50, gravity.xAccel); // X
+            telemetry.update();
+            Thread.sleep(50);
+
+        }
+    }
+
+    public void moveSideways(double power) throws InterruptedException{
+        motorFrontLeft.setPower(power);
+        motorBackLeft.setPower(-power);
+        motorFrontRight.setPower(-power);
+        motorBackRight.setPower(power);
+
+        for(int i=0; i<40; i++){
+//            telemetry.addData("Forward(Z)"+i*50, gravity.zAccel); //Z
+            telemetry.addData("Sideways(Y)"+i*50, gravity.yAccel); // Y
+//            telemetry.addData("Up (X)"+i*50, gravity.xAccel); // X
+            telemetry.update();
+            Thread.sleep(50);
+
+        }
     }
 }
