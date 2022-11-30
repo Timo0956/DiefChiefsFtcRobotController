822a7a0f618832f7f4238ba4d8e57efb7732ec5e
dodobird-ui <68713775+dodobird-ui@users.noreply.github.com>
2022-11-29 22:25:22 -0700

    disabled.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/clubTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/clubTest.java
@@ -1,11 +1,13 @@
 package org.firstinspires.ftc.teamcode.Tele.untested;
 
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
+@Disabled
 public class clubTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException{
