6e0e76d819215bef4dda0d03b965d1dc90f490bb
vityism <wz2684@gmail.com>
2022-12-02 16:21:14 -0700

    sequence created?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/pieceTogether.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/pieceTogether.java
@@ -7,8 +7,21 @@ import org.firstinspires.ftc.teamcode.Tele.untested.servoStuff.ServoTele;
 public class pieceTogether {
     public static void pieceTogether(Gamepad g1) throws InterruptedException {
 
-        ExtensionLinSlide.extendHori();
-        ServoTele.close(g1.y);
+        if(g1.b) {
+            if(TwoStageLinSlideFileNew.state == TwoStageLinSlideFileNew.states.LOW){
+                TwoStageLinSlideFileNew.moveStates(0, false,false, 1, false, false);
+                Thread.sleep(500);
+            }
+            ExtensionLinSlide.extendHori();
+        }
+        else if(g1.y){
+            ServoTele.close(g1.y);
+            Thread.sleep(500);
+            load();
+        }
+
+    }
+    public static void load() throws InterruptedException{
         ExtensionLinSlide.retractHori();
         servo180pullback.placeHolder.setPosition(1);
         TwoStageLinSlideFileNew.moveStates(0, true,false, 0, false, false);
