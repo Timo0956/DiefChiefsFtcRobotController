3881b2f0d577428f049eba2b54bfd17f4b61300b
vityism <wz2684@gmail.com>
2022-12-02 23:02:10 -0700

    teleop changes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/farmMode.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.Tele.untested.newTeleOp;
+
+import org.firstinspires.ftc.teamcode.Tele.untested.servoStuff.ServoTele;
+
+public class farmMode {
+    public static void farm() throws InterruptedException{
+        ExtensionLinSlide.extendHori(); //includes opening claw
+        ServoTele.close(true);
+        Thread.sleep(500);
+        pieceTogether.load();
+        pieceTogether.upDrop();
+        pieceTogether.armDown();
+    }
+}
