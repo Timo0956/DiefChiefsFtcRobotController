1d26fa0c8c39a7a003066e85b66d2f137812b467
Timo <83043823+Timo0956@users.noreply.github.com>
2022-12-09 14:57:49 -0700

    Create acceleration.java
    not completely done yet

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/acceleration.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
+
+@TeleOp
+public class acceleration extends LinearOpMode {
+
+    BNO055IMU imu;
+    Acceleration acceleration;
+    Orientation angles;
+
+    public static float heading = 0;
+    public static float velocity = 0;
+    public static float distance = 0;
+
+    public static accel() {
+
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        angles   = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        acceleration = imu.getAcceleration();
+
+        //Just putting here for later to use. Need to delay to not make it think velocity is extremely fast
+        heading = formatAngle(angles.angleUnit, angles.firstAngle);
+
+        //delay by 1/4 of a second and 1/4 the accel to get velocity
+        velocity += acceleration;
+        distance += velocity;
+    }
+}
