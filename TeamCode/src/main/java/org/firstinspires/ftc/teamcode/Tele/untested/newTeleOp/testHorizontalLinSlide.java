d22e553b28dfff06fd5cd87bd37a354e5e3380fe
jace-wong <k_banh@icloud.com>
2022-12-10 23:56:24 -0700

    what am i doing? no idea will it work? probably not

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/testHorizontalLinSlide.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.teamcode.Tele.untested.newTeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class testHorizontalLinSlide extends LinearOpMode {
+    static DcMotor linSlide;
+    @Override
+
+    public void runOpMode() throws InterruptedException {
+        linSlide = hardwareMap.dcMotor.get("Horizontal LinSlide");
+        linSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        linSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        while(opModeIsActive()) {
+            moveHorizontalLin(gamepad1.left_bumper, gamepad1.right_bumper); //Placeholder Buttons
+        }
+    }
+
+    public enum states {in, out, goIn, goOut};
+    public static states state = states.in;
+
+    public static void moveHorizontalLin(Boolean left, Boolean right) {
+        switch(state) {
+            case in:
+                if(right) {
+                    state = states.goOut;
+                }
+                break;
+            case out:
+                if(left) {
+                    state = states.goIn;
+                }
+                break;
+            case goIn:
+                if(linSlide.getCurrentPosition() > 0) {
+                    linSlide.setTargetPosition(0);
+                    linSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    linSlide.setPower(-1);
+
+                } else {
+                    linSlide.setPower(0);
+                    state = states.in;
+                }
+                break;
+            case goOut:
+                if(linSlide.getCurrentPosition() < 1000) {
+                    linSlide.setTargetPosition(1000);
+                    linSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    linSlide.setPower(1);
+
+                } else {
+                    linSlide.setPower(0);
+                    state = states.out;
+                }
+                break;
+        }
+
+    }
+
+    /*public enum state {
+        in {
+            @Override
+            public state in() {
+
+            }
+        }
+
+        out {
+            @Override
+                    public state out() {
+
+            }
+        }
+
+        goIn {
+            @Override
+                    public state goIn() {
+
+            }
+        }
+
+        goOut {
+            @Override
+                    public state goOut() {
+
+            }
+        }
+    }*/
+}
