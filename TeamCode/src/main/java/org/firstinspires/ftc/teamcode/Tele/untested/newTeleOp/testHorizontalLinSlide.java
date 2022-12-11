3edfe7b035bb62a7e5ae709399333a7df64bed0a
vityism <wz2684@gmail.com>
2022-12-11 13:58:32 -0700

    added @teleop to the testing file

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/testHorizontalLinSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/testHorizontalLinSlide.java
@@ -1,8 +1,9 @@
 package org.firstinspires.ftc.teamcode.Tele.untested.newTeleOp;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-
+@TeleOp
 public class testHorizontalLinSlide extends LinearOpMode {
     static DcMotor linSlide;
     static int counter;
