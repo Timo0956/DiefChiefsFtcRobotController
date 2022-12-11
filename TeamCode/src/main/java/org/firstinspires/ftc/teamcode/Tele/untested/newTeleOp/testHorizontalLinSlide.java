d171a22cb8acc08d481c539b033c458e6d767b43
jace-wong <k_banh@icloud.com>
2022-12-11 13:33:19 -0700

    shitty auto testy thingy?

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/testHorizontalLinSlide.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/newTeleOp/testHorizontalLinSlide.java
@@ -5,6 +5,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class testHorizontalLinSlide extends LinearOpMode {
     static DcMotor linSlide;
+    static int counter;
+    static Boolean isOut;
     @Override
 
     public void runOpMode() throws InterruptedException {
@@ -12,10 +14,30 @@ public class testHorizontalLinSlide extends LinearOpMode {
         linSlide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         linSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         linSlide.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+        counter = 0;
+        isOut = false;
 
         while(opModeIsActive()) {
+            if(counter == 0) {
+                moveHorizontalLinAuto();
+                moveHorizontalLinAuto();
+                counter++;
+            }
             moveHorizontalLinManual(gamepad1.left_bumper, gamepad1.right_bumper); //Placeholder Buttons
+        }
+    }
+
+    public static void moveHorizontalLinAuto() {
+        if(isOut) {
+            linSlide.setTargetPosition(0);
+            linSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            linSlide.setPower(-1);
+            isOut = false;
+        } else {
+            linSlide.setTargetPosition(1000);
+            linSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            linSlide.setPower(1);
+            isOut = true;
         }
     }
 
@@ -29,11 +51,13 @@ public class testHorizontalLinSlide extends LinearOpMode {
                     state = states.goOut;
                 }
                 break;
+
             case out:
                 if(left) {
                     state = states.goIn;
                 }
                 break;
+
             case goIn:
                 if(linSlide.getCurrentPosition() > 0) {
                     linSlide.setTargetPosition(0);
@@ -45,6 +69,7 @@ public class testHorizontalLinSlide extends LinearOpMode {
                     state = states.in;
                 }
                 break;
+
             case goOut:
                 if(linSlide.getCurrentPosition() < 1000) {
                     linSlide.setTargetPosition(1000);
