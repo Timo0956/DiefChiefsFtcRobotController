90998a62b2734280d261cedbec825f9b09049257
jace-wong <k_banh@icloud.com>
2023-01-11 19:37:55 -0700

    i never know how to drive B)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tele/untested/COMPCODEDON/howDrive.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.Tele.untested.COMPCODEDON;
+
+public class howDrive {
+    /*
+    GAMEPAD1
+
+    DRIVING
+    - Left stick vertical to move forward/backward
+    - Left stick horizontal to strafe left/right
+    - Right stick horizontal to turn
+    =========================================================
+    CLAW
+    - Y to close
+    - X to open
+    =========================================================
+    LINSLIDE
+    - Right bumper - Medium junction
+    - Right trigger - High junction
+    - Left bumper - Bottom
+    - Left trigger - Low junction
+    =========================================================
+    AUTO SHIT
+    - Left Dpad - spin 180
+    - Dpad right - spin 180 + High junction
+    - A - Full cycle (Start facing cone, and facing away from target junction)
+    =========================================================
+     */
+}
